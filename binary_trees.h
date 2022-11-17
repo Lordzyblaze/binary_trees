57dd036569d9a5a25b7aa886deb03fbee3ae5be6
Adams Adjartey <melawzy421@yahoo.com>
2022-11-17 10:57:43 +0000

    1. Insert left

--- binary_trees.h
+++ binary_trees.h
@@ -11,14 +11,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
+typedef struct binary_tree_s binary_tree_t;
 
 /** Functions of tasks */
 void binary_tree_print(const binary_tree_t *);
